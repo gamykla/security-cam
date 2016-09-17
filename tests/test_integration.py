c3e7b9238eac7326206533341015b44fd98ececd
gamykla <admin@gamykla.com>
2016-09-17 09:45:56 -0400

    first working test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,4 +1,11 @@
+import httplib
+import json
+import requests
+from nose.tools import eq_
 
 
 def test_x():
-    pass
+    headers = {'Content-Type': 'application/json'}
+    data = {"test": "hello world"}
+    response = requests.post("http://localhost:8080/captures/", headers=headers, data=json.dumps(data))
+    eq_(response.status_code, httplib.CREATED)

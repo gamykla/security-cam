bcee2d3242b4ec13c9621a791368cc28f19f5d49
Justin Simonelis <justin.p.simonelis@gmail.com>
2016-09-17 09:41:49 -0400

    test runner

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,6 @@
 from fabric.api import task, local
 
 DOCKER_REPOSITORY = "jelis/cam_server"
-
 TEST_CONTAINER_NAME = "cam_server"
 
 
@@ -20,3 +19,12 @@ def stop():
     container_id = local('docker ps --filter "name={}" -aq'.format(TEST_CONTAINER_NAME), capture=True)
     local('docker stop {}'.format(container_id))
     local('docker rm {}'.format(container_id))
+
+
+@task
+def test():
+    run()
+    try:
+        local('nosetests tests/')
+    finally:
+        stop()
